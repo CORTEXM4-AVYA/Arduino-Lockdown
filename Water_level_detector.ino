7a892e96c13b865a231237201e36fb3a6ee1a49c
Avinash Bhausaheb Andhale <61452216+CORTEXM4-AVYA@users.noreply.github.com>
2020-09-12 15:58:35 +0530

    Add files via upload
    I have done this project for my home tank of 500 ML.
    I interfaced UV sensor using Arduino for measuring the distance in cm.
    I took readings of distance when tank is full and when tank is empty.
    Empty=87cm    
    Full=17cm
    these two readings are for my Tank you can take you tank recording accordingly.
    I want my result in percentage to show show the level in percentage.
    So i mapped my reading in range (0,100). For 17cm=100% and for 87cm=100%
    Then  suing ESP-8266-01 Module i am collecting the data from sensor and I am uploading data using MQTT protocol on Adafruit broker.

---
+++ Water_level_detector.ino
@@ -0,0 +1,145 @@
+/***************************************************
+  Adafruit MQTT Library ESP8266 Example
+
+  Must use ESP8266 Arduino from:
+    https://github.com/esp8266/Arduino
+
+  Works great with Adafruit's Huzzah ESP board & Feather
+  ----> https://www.adafruit.com/product/2471
+  ----> https://www.adafruit.com/products/2821
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Tony DiCola for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+/************************* WiFi Access Point *********************************/
+
+#define WLAN_SSID       "Redmi"
+#define WLAN_PASS       "avya25042"
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME    "avya25042"
+#define AIO_KEY         "aio_FLYh712w48ViFIRBGoKmXAUeqy3c"
+
+/************ Global State (you don't need to change this!) ******************/
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+// or... use WiFiFlientSecure for SSL
+//WiFiClientSecure client;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+/****************************** Feeds ***************************************/
+
+
+// modified by Shubham Santosh
+// publish to the feed 'alert' 
+Adafruit_MQTT_Publish alertbutton = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/Level");
+
+/*************************** Sketch Code ************************************/
+
+// Bug workaround for Arduino 1.6.6, it seems to need a function declaration
+// for some reason (only affects ESP8266, likely an arduino-builder bug).
+void MQTT_connect();
+void setup() {
+
+  pinMode(2,INPUT);            // reads button press
+  pinMode(0,OUTPUT);         //connected to LED providing status
+  Serial.begin(115200);
+  delay(10);
+  Serial.println(F("Adafruit MQTT demo"));
+
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+
+}
+int duration;
+int distance; 
+int Percentage;
+void loop() {
+ MQTT_connect();
+  // Ensure the connection to the MQTT server is alive (this will make the first
+  // connection and automatically reconnect when disconnected).  See the MQTT_connect
+  // function definition further below.
+  digitalWrite(0, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(0, LOW);
+  duration = pulseIn (2, HIGH);
+  distance = (duration * 0.034) / 2;
+  Percentage=map(distance,17,80,100,0);
+  //Serial.println(distance);
+  //Serial.println(Percentage);
+  
+
+  // this is our 'wait for incoming subscription packets' busy subloop
+  // try to spend your time here
+
+
+  // Now we can publish stuff!
+  Serial.print(F("\nSending level val "));
+  //Serial.print(x);
+  Serial.print("...");
+  if (!alertbutton.publish(Percentage)) {
+    Serial.println(F("Failed"));
+  } else {
+    Serial.println(F("OK!"));
+  }
+  delay(3000);
+
+  // ping the server to keep the mqtt connection alive
+  // NOT required if you are publishing once every KEEPALIVE seconds
+  /*
+  if(! mqtt.ping()) {
+    mqtt.disconnect();
+  }
+  */
+}
+
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
+}
